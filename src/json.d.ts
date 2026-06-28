6bb03cd139a7cd8e0b35e691db99305ff4313a2d
Athanasios Tasis <tasisinfo2@gmail.com>
2026-06-28 16:33:36 +0300

    Revert "made thanos gpt smarter"
    This reverts commit 083425c385984df0b5262353cc78ffec21e7733d.

--- src/json.d.ts
+++
@@ -1,7 +0,0 @@
-// JSON modules are loaded by the bundler (Vite) at runtime. We keep them
-// untyped for the type-checker so it doesn't infer huge literal types from
-// large data files (which OOMs tsc). Consumers cast to the right type.
-declare module '*.json' {
-  const value: unknown;
-  export default value;
-}
